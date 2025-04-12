c26a3e437ff9e22ec828757bf3224423c193d9ae
Giovani Rafael Tavares Braz <giovanibraz02468@gmail.com>
2025-04-12 15:26:33 -0300

    Iniciando projeto
    Iniciando o projeto principal do curso

--- costs/src/App.js
+++ costs/src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Company from './pages/Company'
+import Contact from './pages/Contact'
+import NewProject from './pages/NewProject'
+import Home from './pages/Home'
+import Container from './Layout/Container'
+import NavBar from './Layout/NavBar'
+import Footer from './Layout/Footer'
+import Projects from './pages/Projects'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <NavBar></NavBar>
+      <Container customClass="min-height">
+        <Routes>
+          <Route path='/' element={<Home></Home>}></Route>
+          <Route path='/projects' element={<Projects></Projects>}></Route>
+          <Route path='/company' element={<Company></Company>}></Route>
+          <Route path='/contact' element={<Contact></Contact>}></Route>
+          <Route path='/newproject' element={<NewProject></NewProject>}></Route>
+        </Routes>
+      </Container>
+      <Footer>
+      </Footer>
+    </Router>
   );
 }
 
