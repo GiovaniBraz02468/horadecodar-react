56e6cb7e43df013f2fe6e33eb3017a486cd96871
Giovani Rafael Tavares Braz <giovanibraz02468@gmail.com>
2025-04-26 18:03:27 -0300

    Componente de mensagem
    Criado o componente de mensagem, no qual tem uma contagem de 3 segundos e apaga a mensagem, podendo ser erro/sucesso.

--- costs/src/pages/NewProject.js
+++ costs/src/pages/NewProject.js
@@ -22,7 +22,7 @@ function NewProject(){
         .then((resp) => resp.json())
         .then((dados) => {
             console.log(dados)
-            navigate('/projects', {message: 'Projeto criado com sucesso'})
+            navigate('/projects', { state: { message: 'Projeto criado com sucesso' } })
         })
         .catch((err => console.log(err)))
     }
